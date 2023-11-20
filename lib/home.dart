6ad7bf2392fff14b4ead2432567ef3da9ac85350
unknown <padang.tutorial@gmail.com>
2023-11-20 20:16:17 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Latihan Grid View"),
+        ),
+      ),
+      body: ListView(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Harimau()),
+              );
+            },
+            child: const Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: Hero(
+                      tag: 'Harimau',
+                      child: Image(
+                        image: NetworkImage(
+                            'https://asset.kompas.com/crops/LHRt-0S8mpj3761_Bp-e3lDyf3U=/0x0:993x662/750x500/data/photo/2022/07/17/62d3e4d12d541.jpg'),
+                        fit: BoxFit.fill,
+                        width: 80,
+                        height: 80,
+                      ),
+                    ),
+                    title: Text('Sonu Nigam'),
+                    subtitle: Text('Best of Sonu Nigam Song'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  leading: ClipOval(
+                    child: Image(
+                      image: NetworkImage(
+                          'https://news.tamansafari.com/Newsportalv22ok/upload/26_Harimau_Sumatera_Full.jpg'),
+                      fit: BoxFit.fill,
+                      width: 80,
+                      height: 80,
+                    ),
+                  ),
+                  title: Text('Sonu Nigam'),
+                  subtitle: Text('Best of Sonu Nigam Song'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Harimau extends StatelessWidget {
+  const Harimau({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail Gambar'),
+      ),
+      body: const Center(
+        child: Hero(
+            tag: 'Harimau',
+            child: Image(
+                image: NetworkImage(
+                    'https://news.tamansafari.com/Newsportalv22ok/upload/26_Harimau_Sumatera_Full.jpg'))),
+      ),
+    );
+  }
+}
